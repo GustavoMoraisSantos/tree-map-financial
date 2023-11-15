0b9031cf6abc340054ac049bde54ae9696b112f0
Gustavo Morais Lopes dos Santos <gustavo.morais1414@gmail.com>
2023-11-15 20:06:40 -0300

    feat: adicionado menu

--- app/page.js
+++ app/page.js
@@ -1,28 +1,34 @@
 "use client";
 import MainDrawer from "@/components/Drawer";
 import { useState } from "react";
+import { MenuOutlined } from "@ant-design/icons";
 
 export default function App() {
   const [isOpen, setIsOpen] = useState(true);
   const onClose = () => {
-    setIsOpen(false)
-  }
+    setIsOpen(false);
+  };
   return (
     <div className="app">
       <div className="container">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-        <button type="button" onClick={() => setIsOpen(!isOpen)}>
-          Trigger Drawer
-        </button>
-        <MainDrawer isOpen={isOpen} onCloseDrawer={onClose}/>
+        <div
+          style={{
+            padding: "10px",
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
+          <MenuOutlined onClick={() => setIsOpen(!isOpen)} />
+          <p style={{ color: "grey" }}>
+            Created by{" "}
+            <a href="#" style={{ textDecoration: "none" }}>
+              Gustavo Morais
+            </a>
+          </p>
+        </div>
+        <div style={{ width: "100%", height: "100vh", background: "red" }}>
+        </div>
+        <MainDrawer isOpen={isOpen} onCloseDrawer={onClose} />
       </div>
     </div>
   );
